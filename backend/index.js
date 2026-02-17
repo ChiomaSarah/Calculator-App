0107c84178cf9beb84581621d145d10244414d92
Osuji Chioma Sarah <osujisarah@gmail.com>
2026-02-17 13:11:14 +0100

    backend: test CORS with wildcard origin

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
     allowedHeaders: "Content-Type, Authorization",
   }),
